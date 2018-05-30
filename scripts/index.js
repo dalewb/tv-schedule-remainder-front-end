6c861ab942a1045445e142d85583e6057e7947a5
myungbeans <michael.h.myung@gmail.com>
2018-05-30 11:56:54 -0400

    fine tuning error msg on dashboard table so that error msg won't reset the start and end times

--- scripts/index.js
+++ scripts/index.js
@@ -11,21 +11,25 @@ document.addEventListener("DOMContentLoaded", (event)=>{
         let start_time = document.getElementById("start_time").value
         let end_time = document.getElementById("end_time").value
         let error_msg = document.getElementById("table_error")
-        if (error_msg){
-            error_msg.remove()
-        }
+        let table = document.querySelector(`body > div > div > main > div.table-responsive > table > tbody`)
 
         if (timeObj.differenceInMinutes(start_time, end_time) < 0) {
-            start_time.innerHTML = "12:00 AM"
-            end_time.innerHTML = "12:00 AM"
+            // debugger;
             let tableSettings = document.getElementById("table_settings")
-            tableSettings.innerHTML += 
+            let error = document.createElement("div")
+            error.setAttribute("id", "table_error")
+            error.innerHTML =
                 `<div id="table_error" class="alert alert-danger">
                 <strong> ERROR </strong> Your start time must come before your end time.
-                </div>`
+                </div>`;
+            tableSettings.appendChild(error)
+            table.innerHTML = ""
+        } else {
+            if (error_msg){
+                error_msg.remove()
+            }
+            table.innerHTML = ""
+            table.innerHTML += timeObj.customizeSchedule(start_time, end_time)
         }
-        let table = document.querySelector(`body > div > div > main > div.table-responsive > table > tbody`)
-        table.innerHTML = ""
-        table.innerHTML += timeObj.customizeSchedule(start_time, end_time)
     })    
 })
