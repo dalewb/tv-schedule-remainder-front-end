f249e622cf8828291a44411b367654db3d32f011
myungbeans <michael.h.myung@gmail.com>
2018-05-30 13:59:22 -0400

    working on rendering search page from homepage

--- scripts/index.js
+++ scripts/index.js
@@ -7,29 +7,35 @@ document.addEventListener("DOMContentLoaded", (event)=>{
 
     //DYNAMICALLY ALTERING TABLE
     table_settings.addEventListener("change", (e) => {
-        let timeObj = new Time
+        let time_obj = new Time
         let start_time = document.getElementById("start_time").value
         let end_time = document.getElementById("end_time").value
         let error_msg = document.getElementById("table_error")
         let table = document.querySelector(`body > div > div > main > div.table-responsive > table > tbody`)
 
-        if (timeObj.differenceInMinutes(start_time, end_time) < 0) {
+        if (time_obj.differenceInMinutes(start_time, end_time) < 0) {
             // debugger;
-            let tableSettings = document.getElementById("table_settings")
+            let table_settings = document.getElementById("table_settings")
             let error = document.createElement("div")
             error.setAttribute("id", "table_error")
             error.innerHTML =
                 `<div id="table_error" class="alert alert-danger">
                 <strong> ERROR </strong> Your start time must come before your end time.
                 </div>`;
-            tableSettings.appendChild(error)
+            table_settings.appendChild(error)
             table.innerHTML = ""
         } else {
             if (error_msg){
                 error_msg.remove()
             }
             table.innerHTML = ""
-            table.innerHTML += timeObj.customizeSchedule(start_time, end_time)
+            table.innerHTML += time_obj.customizeSchedule(start_time, end_time)
         }
-    })    
+    })
+
+    //RENDER SEARCH PAGE
+    let search_button = document.getElementById("search")
+    search_button.addEventListener("click", (e) => {
+        console.log("You've hit search button")
+    })
 })
