c902e18cb1f7c39faa1b99735d3742e10c9a0943
William Dale <williambdale@gmail.com>
2018-06-01 12:20:41 -0400

    Complete rendering episodes on schedule

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -29,5 +29,67 @@ function loadHomePageJS(){
             table.innerHTML = ""
             table.innerHTML += time_obj.customizeSchedule(start_time, end_time)
         }
+
+        function renderSortedEpisode(episodes) {
+
+          episodes.forEach(episode => {
+            let dayNum = 1
+            let stringTime = episode.view_time.split(' ')[0]
+            let amPm = episode.view_time.split(' ')[1]
+            let intTime = parseInt(stringTime)
+            if (parseInt(stringTime) == 0) {
+              stringTime = '12:00'
+              amPm = 'AM'
+            }
+            let pText = `<p><strong>${episode.title}</strong></p>`
+            let strong = document.createElement('strong')
+            strong.innerText = episode.title
+            let pTitle = document.createElement('p')
+            pTitle.append(strong)
+
+            let pSpace = document.createElement('p')
+            let img = document.createElement('img')
+            img.setAttribute('src', `${episode.img_url}`)
+            img.setAttribute("height", "100px")
+            img.setAttribute("width", "200px")
+            pSpace.append(img)
+            pSpace.append(pTitle)
+            space = document.getElementById(`${stringTime}_${amPm}_1`)
+            if (space && space.children.length == 0) {
+              space.append(pSpace)
+            } else if (space) {
+              while (space.children.length > 0) {
+                space = document.getElementById(`${stringTime}_${amPm}_${dayNum}`)
+                dayNum += 1
+              }
+              space.append(pSpace)
+            }
+          })
+        }
+
+        function sortEpisodesByTime(episodes) {
+          let sorted = episodes.sort(function(a,b){
+            return parseInt(a.view_time) - parseInt(b.view_time);
+          })
+          return sorted
+        }
+
+        function sortByAmAndPm(episodes) {
+          let am = episodes.filter(function(ep) {
+            return ep.view_time.toLowerCase().includes("am")
+          })
+          let pm = episodes.filter(function(ep) {
+            return ep.view_time.toLowerCase().includes("pm")
+          })
+          let sortedAm = sortEpisodesByTime(am)
+          let sortedPm = sortEpisodesByTime(pm)
+          let sortedAll =  sortedAm.concat(sortedPm)
+
+          renderSortedEpisode(sortedAll)
+        }
+
+        fetch("http://localhost:3000/api/v1/users/1/episodes")
+          .then(res => res.json())
+          .then(json => sortByAmAndPm(json))
     })
 }
